060c843811360c04fca90476758bca6058a51645
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-04-23 23:19:23 -0400

    Complete

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
+}
+Person.prototype.eat = function(food) {
+  if (this.stomach.length < 10) {
+    return this.stomach.push(food)
+  }
+}
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,8 +70,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0
+  this.odometer = 0
+}
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons
+}
+Car.prototype.drive = function(distance) {
+  let maxDistance = this.milesPerGallon * this.tank;
+  
+  if (distance > 0 && distance <= maxDistance) {
+    this.odometer += distance;
+    return this.tank = this.tank - (distance / this.milesPerGallon);
+  }
+  
+  if (distance > maxDistance) {
+    this.odometer += maxDistance;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
 }
 
 /*
@@ -68,18 +101,29 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+}
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  
+  The four principles presented for determining what "this" in JavaScript 
+  points to boil down to one simple question: 
+  ->* What is the calling object? *<-
+  1. Is the function called by new?
+  2. Is the function called by call(), apply(), or bind()?
+  3. Is the function called as a method, ie: obj.func()?
+  4. Is the function called in the global scope?
+    If strict mode is enabled, return undefined.
+    Otherwise, return the global object, ie: window.
 */
 
 
